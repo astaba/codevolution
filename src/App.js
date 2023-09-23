80948125d9e00a8db455c96bb952a8a7e1227aa8
Astar <bahouidi99@gmail.com>
2023-09-23 08:01:08 +0100

    Hook up react-query

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import { Link, Route, Routes } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
 
 import "./App.css";
 import SuperHeroes from "./components/SuperHeroes.page";
 import RQSuperHeroes from "./components/RQSuperHeroes.page";
 import Home from "./components/Home.page";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/super-heroes">Traditional Super Heroes</Link>
-          </li>
-          <li>
-            <Link to="/rq-super-heroes">RQ Super Heroes</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/super-heroes" element={<SuperHeroes />} />
-        <Route path="/rq-super-heroes" element={<RQSuperHeroes />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/super-heroes">Traditional Super Heroes</Link>
+            </li>
+            <li>
+              <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/super-heroes" element={<SuperHeroes />} />
+          <Route path="/rq-super-heroes" element={<RQSuperHeroes />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
