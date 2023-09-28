f9fbca60a882629d7f9a977159b61e0052daec28
Astar <bahouidi99@gmail.com>
2023-09-28 15:57:44 +0100

    Infinite queries

--- src/components/RQInifiniteQueries.page.js
+++ src/components/RQInifiniteQueries.page.js
@@ -1,12 +1,39 @@
-import { useQuery } from "@tanstack/react-query";
+import { Fragment } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+// Set json server pagination limit
+const limit = 2;
+
 const RQInifiniteQueries = () => {
-  const { status, error, data } = useQuery({
+  const {
+    status,
+    error,
+    data,
+    hasNextPage,
+    fetchNextPage,
+    isFetching,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
     queryKey: ["colors"],
-    queryFn: () => {
-      return axios.get("http://localhost:4000/colors");
+    queryFn: ({ pageParam = 1 }) => {
+      return axios.get(
+        `http://localhost:4000/colors?_limit=${limit}&_page=${pageParam}`
+      );
     },
+    getNextPageParam: (lastPage, pages) => {
+      // compute maxPage base on a response header from json server
+      const maxPage = Math.ceil(lastPage.headers["x-total-count"] / limit);
+      if (pages.length < maxPage) return pages.length + 1;
+      return undefined;
+    },
+    // To display data in reverse order
+    // select: (data) => {
+    //   return {
+    //     pages: [...data.pages].reverse(),
+    //     pageParams: [...data.pageParams].reverse(),
+    //   };
+    // },
   });
 
   console.log(data);
@@ -20,15 +47,32 @@ const RQInifiniteQueries = () => {
         <h3>Error: {error.message}</h3>
       ) : (
         <ul>
-          {data?.data.map((color) => (
-            <li key={color.id}>
-              <h4>
-                {color.id} - {color.label}
-              </h4>
-            </li>
+          {data?.pages.map((group, i) => (
+            <Fragment key={i}>
+              {group.data.map((color) => (
+                <li key={color.id}>
+                  <h4>
+                    {color.id} - {color.label}
+                  </h4>
+                </li>
+              ))}
+            </Fragment>
           ))}
         </ul>
       )}
+      <div>
+        <button
+          onClick={() => fetchNextPage(/*{ pageParam: 3 }*/)} // To pass data to queryFn
+          disabled={!hasNextPage}
+        >
+          {isFetchingNextPage
+            ? "Loading more..."
+            : hasNextPage
+            ? "Load more"
+            : "Nothing more to load"}
+        </button>
+      </div>
+      {isFetching && !isFetchingNextPage ? <p>Fetching...</p> : null}
     </>
   );
 };
