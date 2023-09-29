25ed8ea6df75527ab2b0e4892c1dd9f87e5523fd
Astar <bahouidi99@gmail.com>
2023-09-29 10:57:00 +0100

    Query invalidation:
    Tell react-query to refetch the specified query after server-side data update in this case performed by the mutation

--- src/hooks/useAddSuperHeroes.js
+++ src/hooks/useAddSuperHeroes.js
@@ -1,10 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 
 const useAddSuperHeroes = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (payload) => {
       return axios.post("http://localhost:4000/superheroes", payload);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["super-heroes"] });
     },
   });
 };
